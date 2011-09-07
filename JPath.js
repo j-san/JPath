d338ea5aacf2d99b5acd35e67380ec2bd533a88e
Jonathan Sanchez <jonathansanchez@me.com>
2011-09-07 22:06:52 +0200

    Integrate expression computing in path resolution

--- JPath.js
+++ JPath.js
@@ -1,31 +1,119 @@
 (function()
 {
-    var getInside = function(data, callback)
-    {
-        if (data instanceof Array)
-        {
-            for (var i = 0; i < data.length; i++)
-            {
-                if (data[i] instanceof Array)
-                {
-                    getInside(data[i],callback);
-                }
-                else 
-                {
-                    callback(data[i]);
-                }
-            }
+    var analyseExpression = function(expression/*,root*/){
+	//console.log(expression);
+	var separators = /^([\/\w\*\[\]]*)\s+(\+|\/|-|\*|div|mod|\||or|and|\(|\[|=)\s+(.*)$/;
+	var parts = expression.match(separators);
+	
+	if(parts && parts.length>1)
+	{
+	    //console.log('path',parts);
+	    var separator = parts[2].trim();
+	    if(separator == '[' || separator == '(')
+	    {
+		//console.log('cond',parts);
+		var pos = parts[1].length + 1;
+		var c = 1;
+		while((c > 0 || parts[2] != '[') && parts[3]){
+		    parts = parts[3].match(/^([\/\w]*)\s*(\]|\[)\s*(.*)$/);
+		    console.log(parts);
+		    if(parts && parts.length > 2)
+		    {
+			pos += parts[1].length + 1;
+			if(parts[2] == ']')
+			{
+			    c--;
+			}
+			else
+			{
+			    c++;
+			}
+			console.log(c);
+		    }
+		    else
+		    {
+			throw Error('Syntax error:'+expression);
+		    }
+		    
+		}
+		if(c != 0)
+		{
+		    throw Error('Syntax error');
+		}
+		var path = expression.substring(0, pos);
+		console.log(path,parts);
+		if(parts[3])
+		{
+		    parts = expression.match(parts[3]);
+		    if(!parts[2])
+		    {
+			throw Error('Syntax error ' + expression);
+		    }
+		    var operation = {
+			left : path,
+			operator : parts[1].trim(),
+			right : analyseExpression(parts[2])
+	    
+		    }
+		    //console.log(expression,JSON.stringify(operation));
+		    
+		    operation = resolvePriority(operation);
+	    
+		    //console.log(expression,JSON.stringify(operation));
+		   return operation;
+		}
+		else
+		{
+		    return path;
+		}
+	    }
+	    if(!parts[3])
+	    {
+		throw Error('Syntax error ' + expression);
+	    }
+	    var operation = {
+		left : parts[1],
+		operator : separator,
+		right : analyseExpression(parts[3])
+    
+	    }
+	    //console.log(expression,JSON.stringify(operation));
+	    
+	    operation = resolvePriority(operation);
+    
+	    //console.log(expression,JSON.stringify(operation));
+	   return operation;
+	}
+	else
+	{
+	    return expression;
+	}
+    }
+
+    var evaluatePath = function(path, data, rootJPathSet){
+        //var m = /$(\/?\/?\w*(?!\[\w*\])?)(.+)?^/.match(p);
+        var m = path.match(/^(\/?\/?(?:\w+|\*)(?:\[.*\])*)(\/.*)?$/);
+        // console.log('parse', m, data);
+        
+        if(!m)
+	{
+            throw Error('syntax error ' + path);
         }
-        else
+        else if (m[2] && m[2] != '/')
         {
-            callback(data);
+            return evaluatePath(m[2], filterSet(data, m[1]), rootJPathSet);
         }
+        else if (m[1])
+        {
+            return JPath(filterSet(data, m[1]), rootJPathSet);
+        }
+        return null;
     }
 
     var filterSet = function(set, filter, deep)
     {
 	var results = [];
-	var parts = filter.split(/(?:\]|\[)/);
+	var parts = filter.split(/(?:\]|\[)/); 
 	var name = parts.shift();
 	if(name[1] == '/')
 	{
@@ -103,7 +191,7 @@
 		{
 		    for (i = 0; i < results.length; i++)
 		    {
-			var subset = query(results[i],cond);
+			var subset = query(cond,results[i]/*,root*/);
 			if(subset.length)
 			{
 			    results2.push(results[i]);
@@ -114,6 +202,28 @@
 	    }
 	}
         return results;
+    }
+
+    var getInside = function(data, callback)
+    {
+        if (data instanceof Array)
+        {
+            for (var i = 0; i < data.length; i++)
+            {
+                if (data[i] instanceof Array)
+                {
+                    getInside(data[i],callback);
+                }
+                else 
+                {
+                    callback(data[i]);
+                }
+            }
+        }
+        else
+        {
+            callback(data);
+        }
     }
 
     var values = function(set)
@@ -137,31 +247,27 @@
 
 	return data;
     }
-
-    var query = function(data, path) {
-	if(!path){
-	    return JPath(data);
-	}
-        //var m = /$(\/?\/?\w*(?!\[\w*\])?)(.+)?^/.match(p);
-        var m = path.match(/^(\/?\/?(?:\w+|\*)(?:\[.*\])*)(\/.*)?$/);
-        // console.log('parse', m, data);
-        
-        if(!m)
+    
+    var resolvePriority = function(operation)
+    {
+	if(priority[operation.operator] - priority[operation.right.operator] <= 0)
 	{
-            throw Error('syntax error');
-        }
-        else if (m[2] && m[2] != '/')
-        {
-            return query(filterSet(data, m[1]), m[2]);
-        }
-        else if (m[1])
-        {
-            return JPath(filterSet(data, m[1]));
-        }
-        return null;
-    };
+	    opcopy = operation.right;
+	    operation.right = opcopy.left;
+	    opcopy.left = operation;
+	    operation = opcopy;
+	    if(typeof operation.left == 'object')
+	    {
+		operation.left = resolvePriority(operation.left);
+	    }
+	}
+	return operation;
+    }
 
-    window.JPath = function(data) {
+    
+
+    /**** MAIN JPATH OBJECT DECLARATION ****/
+    window.JPath = function(data,root) {
         var set;
         if (data instanceof Array)
         {
@@ -172,10 +278,14 @@
             set = [];
             if (data){ set.push(data); }
         }
+	
+	if(root){
+	    set.root = root;
+	}
 
 
         set.q = function(path,callback) {
-	    var results = query(set, path);
+	    var results = query(path,set,root);
 	    if(callback){
 		for (var i = 0; i < results.length; i++)
 		{
@@ -188,7 +298,7 @@
 	set.query = set.q;
 
          set.count = function(path) {
-            return query(set, path).length;
+            return query(path,set,root).length;
         };
 
         set.sum = function(path) {
@@ -199,19 +309,133 @@
 	    return sum;
         };
 
-         set.exists = function(path) {
-            return query(set, path).length > 0;
+        set.exists = function(path) {
+            return query(path,set,root).length > 0;
         };
 
-         set.valueOf = function(path) {
-	    return values(query(set, path)).join();
+        set.valueOf = function(path) {
+	    return values(query(path,set,root)).join();
         };
 
-         set.copyOf = function(path) {
-            return JSON.stringify(query(set, path));
+        set.copyOf = function(path) {
+            return JSON.stringify(query(path,set,root));
         };
-
 
         return set;
     };
+    
+    /**
+     * @param path a string
+     * @param data an Array or Object to query
+     * @param root a JPath object
+     */
+    var query = function(path,data,root) {
+	if(!path)
+	{
+	    return data;
+	}
+
+	var operate = function(operation)
+	{
+	    if(typeof operation == 'string')
+	    {
+		var i = parseInt(operation);
+    
+		if(isNaN(i) || i.toString() != operation)
+		{
+		    return evaluatePath(operation,data,root);
+		}
+		return i;
+	    }
+	    else
+	    {
+		return operateFunctions[operation.operator](operation.left,operation.right);
+	    }
+	}
+    
+	var operateAdd = function(left,right)
+	{
+	    return castInt(operate(left)) + castInt(operate(right));
+	}
+	
+	var operateMult = function(left,right)
+	{
+	    return castInt(operate(left)) * castInt(operate(right));
+	}
+	
+	var operateDiv = function(left,right)
+	{
+	    return castInt(operate(left)) / castInt(operate(right));
+	}
+	
+	var operateMod = function(left,right)
+	{
+	    return castInt(operate(left)) % castInt(operate(right));
+	}
+	
+	var operateSub = function(left,right)
+	{
+	    return castInt(operate(left)) - castInt(operate(right));
+	}
+	
+	var operateEquals = function(left,right)
+	{
+	    return operate(left) == operate(right);
+	}
+    
+	var operateJoin = function(left,right)
+	{
+	    return operate(left).concat(operate(right));
+	}
+
+
+	var operateFunctions = {
+	    '+':operateAdd,
+	    '-':operateSub,
+	    '*':operateMult,
+	    'div':operateDiv,
+	    '/':operateDiv,
+	    'mod':operateMod,
+	    '=':operateEquals,
+	    '|':operateJoin
+	    // and
+	    // or
+	};
+	
+	return operate(analyseExpression(path))
+    };
+
+    
+
+    var castInt = function(value)
+    {
+	if(value instanceof Array)
+	{
+	    return value[0];
+	}
+	return value;
+    }
+
+    var castBool = function(value)
+    {
+	if(value instanceof Array)
+	{
+	    return value.length;
+	}
+	return value;
+    }
+
+    var priority = {
+	'mod':1,
+	'*':3,
+	'div':3,
+	'/':3,
+	'+':5,
+	'-':5,
+	'|':7,
+	'=':9,
+	'and':12,
+	'or':15
+    };
+
 } ());
