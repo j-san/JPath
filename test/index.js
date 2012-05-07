60739a32ef1a28e712529cde969a2d7259756f97
Jonathan Sanchez <jonathansanchez@me.com>
2012-05-07 22:57:30 +0200

    node.js complient

---
+++ test/index.js
@@ -0,0 +1,15 @@
+const qunit = require('qunit');
+
+qunit.run({
+
+    code: {
+        /* Include your CODE to test here */
+        path: './src/JPath.js',
+
+    },
+    tests: [
+        /* Include your TESTS to run here */
+        'test.js'
+
+    ].map(function (v) { return './test/' + v })
+});
